e4ee5aedceb65d8404d8e06351ccbcdadd55f62b
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-04-17 16:38:35 -0300

    Create aula11.py

---
+++ Teste/aula11.py
@@ -0,0 +1,6 @@
+print('\033[0;30;41mTeste\033[m') 
+print('\033[4;33;44mTeste\033[m') 
+print('\033[1;35;43mTeste\033[m') 
+print('\033[30;42mTeste\033[m') 
+print('\033[mTeste\033[m') 
+print('\033[7;30mTeste\033[m') 
