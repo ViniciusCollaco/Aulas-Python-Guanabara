14d2a3244aee097bbf109f54dc9fc56ef75c93ce
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-04-20 17:20:25 -0300

    Update aula11.py

--- Teste/aula11.py
+++ Teste/aula11.py
@@ -5,6 +5,11 @@ print('\033[30;42mTeste\033[m')
 print('\033[mTeste\033[m') 
 print('\033[7;30mTeste\033[m') 
 
+#Exemplo de construção de cor nos programas
 a = 3
 b = 5
 print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m!!!'.format(a, b))
+
+#Outra madeira de executar usando .format
+nome = 'Vinicius'
+print('Olá!')
