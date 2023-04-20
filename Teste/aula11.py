12a1a7db596a51ebcef5fed6cad81f9c3d981d42
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-04-20 15:28:09 -0300

    Update aula11.py

--- Teste/aula11.py
+++ Teste/aula11.py
@@ -4,3 +4,7 @@ print('\033[1;35;43mTeste\033[m')
 print('\033[30;42mTeste\033[m') 
 print('\033[mTeste\033[m') 
 print('\033[7;30mTeste\033[m') 
+
+a = 3
+b = 5
+print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m!!!'.format(a, b))
