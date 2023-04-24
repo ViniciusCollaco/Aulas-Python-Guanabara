08df98ddc6ec6e48881da25a51e2fe435762f597
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-04-24 15:26:44 -0300

    Update aula11.py

--- Teste/aula11.py
+++ Teste/aula11.py
@@ -12,4 +12,13 @@ print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m!!!'.format(a, b))
 
 #Outra madeira de executar usando .format
 nome = 'Vinicius'
-print('Olá!')
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome, '\033[m'))
+
+#outro exemplo de como executar 
+nome = 'Vinicius'
+cores = {'limpa':'\033[m', 
+         'azul':'\033[34m', 
+         'amarelo':'\033[33m', 
+         'pretoebranco':'\033[7;30m'}
+
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format(cores['amarelo'], nome, cores['limpa']))
