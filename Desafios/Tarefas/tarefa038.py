38c2b8113eb9f408231320c972f8ebce7e77c255
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-05-11 13:14:48 -0300

    Update tarefa038.py
    Execucao do 38 exercicio

---
+++ Desafios/Tarefas/tarefa038.py
@@ -0,0 +1,8 @@
+valor1 = int(input('Qual o primeiro valor: '))
+valor2 = int(input('Qual o segundo valor: '))
+if valor1 > valor2:
+    print('O primeiro valor é maior.')
+elif valor2 > valor1:
+    print('O segundo valor é maior') 
+else:
+    print('Não existe valor maior, os dois são iguais.')
