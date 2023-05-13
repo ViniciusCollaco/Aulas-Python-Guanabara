ab7c53dae5d219643c6d14ea777c30449f85e615
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-05-13 15:02:47 -0300

    Update tarefa042.py
    Corrigindo logica de commparação

--- Desafios/Tarefas/tarefa042.py
+++ Desafios/Tarefas/tarefa042.py
@@ -3,9 +3,9 @@ a = float(input('Qual o primeiro segmento: '))
 b = float(input('Qual o segundo segmento: '))
 c = float(input('Qual o terceiro segmento: '))
 if a + b > c and a + c > b and b + c > a :
-    if a == b == c:
+    if a == b == c == a:
         print('Os segmentos acima PODEM formar um triângulo EQUILÁTERO.')
-    elif a != b != c:
+    elif a != b != c != a:
         print('Os segmentos acima PODEM formar um triângulo ESCALENO.')
     else:
         print('Os segmentos acima PODEM formar um triângulo ISÓSCELES.')
