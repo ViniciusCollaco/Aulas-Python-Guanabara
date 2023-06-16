fd1a7309e2297aa4b44fc0d154c8021716637ba0
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-06-16 15:28:07 -0300

    Update tarefa056.py
    Exclusão recriação do exercicio

--- Desafios/Tarefas/tarefa056.py
+++ Desafios/Tarefas/tarefa056.py
@@ -1,30 +1,23 @@
-genero = sexo.upper()
-acumulador = 0
-for contagem in range(1,5):
-    print('Digite a {}° pessoa'.format(contagem))
-    nome = str(input('Qual é seu nome: '))
-    idade = int(input('Qual é sua idade: '))
-    sexo = str(input('Qual o seu sexo (M/F): '))
-    media =+ idade
-    if genero == F and idade <= 20:
-        acumulador =+ 1
-    else:
+somaIdade = 0
+mediaIdade = 0
+maiorIdade = 0
+nomeMaior = ''
+acumuladorMulher = 0
+for contador in range (1, 5):
+    print('----- Digite o nome da {}° pessoa -----'.format(contador))
+    nome = str(input('Qual é seu nome: ')).strip()
+    idade = int(input('Qual sua idade: '))
+    sexo = str(input('Qual é seu sexo (M/F): ')).strip()
+    somaIdade =+ idade
+    if contador == 1 and sexo in 'Mm':
+        maiorIdade = idade
         nomeMaior = nome
-        maior = idade
-        nomeMenor = nome
-        menor = idade
-        if idade > maior:
-            maior = idade
-            nomeMaior = nome
-        if idade < menor:
-            menor = idade
-            nomeMenor = nome
-print('A media de idade é {}'.format(media/4))
-print('O homem mais velho é {} com {} anos'.format(nomeMaior, maior))  
-print('O homem mais novo é {} com {} anos'.format(nomeMenor, menor)) 
-print('Existe {} mulheres com idade ate 20 anos'.format(acumulador))       
-    
-        
-    
-    
-    
+    if sexo in 'Mn' and idade > maiorIdade:
+        maiorIdade = idade
+        nomeMaior = nome
+    if sexo in 'Ff' and idade < 20:
+        acumuladorMulher += 1
+mediaIdade = somaIdade / 4
+print('A média de idade do grupo é de {} anos'.format(mediaIdade))
+print('O homem mais velho tem {} anos e se chama {}'.format(maiorIdade, nomeMaior))
+print('Ao todo são {} mulheres com menos de 20 anos'.format(acumuladorMulher))
