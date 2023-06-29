e75b4a251451aeb2e6a0885f0005acc753a35718
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-06-29 16:37:54 -0300

    Update tarefa059.py

--- Desafios/Tarefas/tarefa059.py
+++ Desafios/Tarefas/tarefa059.py
@@ -1,9 +1,9 @@
+from time import sleep
 valorA = int(input('Qual o primeiro valor: '))
 valorB = int(input('Qual o segundo valor: '))
 print('[1]Somar \n[2]Multiplicar \n[3]Maior \n[4]Novos números \n[5]Sair do programa')
 opcao = int(input('Qual é a sua opção? '))
 while opcao != 5:
-    print('=-=' * 10)
     if opcao == 1:
         soma = valorA + valorB
         print('A soma entre {} + {} é {}'.format(valorA, valorB, soma))
@@ -23,6 +23,7 @@ while opcao != 5:
     else:
         print('Opção invalida, tente novamente')
     print('=-=' * 10)
+    sleep(1)
     print('[1]Somar \n[2]Multiplicar \n[3]Maior \n[4]Novos números \n[5]Sair do programa')
     opcao = int(input('Qual é a sua opção?'))
 print('Programa encerrado. Obrigado pelo uso!!!')
