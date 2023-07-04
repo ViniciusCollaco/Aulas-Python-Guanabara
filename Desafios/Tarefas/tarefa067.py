fd22a203163109a8bb2d1b78b5a361df1a9e1641
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-07-04 15:26:31 -0300

    Create tarefa067.py
    Tabuada, com saida de numero negativo

---
+++ Desafios/Tarefas/tarefa067.py
@@ -0,0 +1,12 @@
+valor = mult = 0
+print('Tabuada, caso queira sair colocar numero negativo.')
+while valor >= 0:
+    print('-=-' * 30)
+    valor = int(input('Qual valor desejado para a tabuada: '))
+    print('-=-' * 30)
+    if valor < 0:
+        print('Programa encerrado, obrigado por usar!!!')
+        break
+    for cont in range (1, 11):
+        mult = cont * valor
+        print(f'{valor} x {cont} = {mult}')
