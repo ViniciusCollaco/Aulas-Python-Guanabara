67397f47c2b144225171ccf164af17274f15d4df
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-07-04 15:11:26 -0300

    Create tarefa066.py

---
+++ Desafios/Tarefas/tarefa066.py
@@ -0,0 +1,11 @@
+valor = cont = soma =0
+while valor != 999:
+    print('-=-' * 30)
+    valor = int(input('Qual valor que voce deseja (digite 999 para parar) '))
+    if valor == 999:
+        break
+    cont += 1
+    soma += valor
+print('-=-' * 30)
+print(f'Foram registrados {cont} e o total foi de {soma}')
+    
