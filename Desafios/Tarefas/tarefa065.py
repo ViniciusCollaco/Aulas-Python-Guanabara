6e93f1a534c9d445f8ec8f0326a943cd4a1ae5c8
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-07-04 14:31:24 -0300

    Update tarefa065.py

--- Desafios/Tarefas/tarefa065.py
+++ Desafios/Tarefas/tarefa065.py
@@ -16,4 +16,4 @@ while pergunta in 'Ss':
 media = acumulador / cont
 print('A media dos valores foi de {:.2f}, foi no total de {} numeros, o maior numero é {}, e o menor numero é {}'.format(media, cont, maior, menor))
 
-'''print(f'A media dos valores foi de {media:.2f}, foi no total de {cont} numeros, o maior numero é {maior}, e o menor numero é {menor}')'''
+'''print(f'A media dos valores foi de {media:.2f}, foi no total de {cont} numeros, o maior numero é {maior}, e o menor numero é {menor}')'''    
