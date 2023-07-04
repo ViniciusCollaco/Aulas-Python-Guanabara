c30eb547a1e2a663dcd3ecc0e1b2f5f30775c274
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-07-04 15:32:24 -0300

    Update tarefa067.py

--- Desafios/Tarefas/tarefa067.py
+++ Desafios/Tarefas/tarefa067.py
@@ -5,8 +5,8 @@ while True:
     valor = int(input('Qual valor desejado para a tabuada: '))
     print('-=-' * 30)
     if valor < 0:
-        print('Programa encerrado, obrigado por usar!!!')
         break
     for cont in range (1, 11):
         mult = cont * valor
         print(f'{valor} x {cont} = {mult}')
+print('Programa encerrado, obrigado por usar!!!')
