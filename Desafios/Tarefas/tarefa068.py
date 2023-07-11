71135b07461d4f4bdc52f05a4c5604e5ed400d2b
Vinicius Collaço <viniciuscollaco@hotmail.com>
2023-07-11 14:03:29 -0300

    Update tarefa068.py

--- Desafios/Tarefas/tarefa068.py
+++ Desafios/Tarefas/tarefa068.py
@@ -1,13 +1,14 @@
 from random import randint
+v = 0
 while True:
     jogador = int(input('Digite um valor: '))
     computador = randint(0,11)
     total = jogador + computador
     tipo = ' '
     while tipo not in 'PI':
-        tipo = str(input('Par ou Impar? [P/I]')).strip() .upper()[0]
+        tipo = str(input('Par ou Impar? [P/I] ')).strip() .upper()[0]
     print(f'Voce jogou {jogador} e o computador {computador}. Total de {total}', end='')
-    print('DEU PAR' if total % 2 == 0 else 'DEU IMPAR')
+    print(' DEU PAR' if total % 2 == 0 else ' DEU IMPAR')
     if tipo == 'P':
         if total % 2 == 0:
             print('Voce VENCEU!')
