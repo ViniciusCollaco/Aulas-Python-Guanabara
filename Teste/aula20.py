87c5ea2cc84a8573891b203487c7d1185126c1a3
Vinicius Collaço <viniciuscollaco@hotmail.com>
2024-01-05 21:07:22 -0300

    aula 20

---
+++ Teste/aula20.py
@@ -0,0 +1,26 @@
+'''def soma(a, b):
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma A + B = {s}')
+# Programa Principal
+soma(b=4, a=5)'''
+
+'''def contador(* num):
+    tam = len(num)
+    print(f'Recebi os valores {num} e são ao todo {tam} numeros')
+# Programa Principal
+contador(2, 1, 7)
+contador(8, 0)
+contador(4, 4, 7, 6, 2)'''
+
+'''def dobra(lst):
+    pos = 0
+    while pos < len(lst):
+        lst[pos] *= 2
+        pos += 1
+# Programa Principal
+valores = [6, 3, 9, 1, 0, 2]
+dobra(valores)
+print(valores)'''
+
+
