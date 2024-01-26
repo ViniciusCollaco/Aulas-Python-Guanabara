52437bc6d32317c3291c056239f0362ee3d97265
Vinicius Collaço <viniciuscollaco@hotmail.com>
2024-01-26 19:26:00 -0300

    aula 21

--- Teste/aula21.py
+++ Teste/aula21.py
@@ -1,3 +1,22 @@
 #help(print)
 
-print(input.__doc__)
+#print(input.__doc__)
+
+'''def somar(a=0, b=0, c=0):
+    """
+    -> Faz a soma de três valores e mostra o resultado na tela.
+    :Parâmetros a: O primeiro valor.
+    :Parâmetros b: O segundo valor. 
+    :Parâmetros c: O terceiro valor.
+    """
+    s = a + b + c
+    print(f'A soma vale {s}')
+
+somar(3, 2)'''
+
+def funcao():
+    n1 = 4
+    print(f'N1 dentro vale {1}')
+    
+n1 = 2
+print(f'N1 global vale {n1}')
