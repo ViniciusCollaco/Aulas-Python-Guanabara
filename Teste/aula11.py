bef0a958eb0c34dd9fd44436efbf0554957429a1
Vinicius Collaço <viniciuscollaco@hotmail.com>
2024-05-27 11:28:04 -0300

    alteraçoes exercicio 11

--- Teste/aula11.py
+++ Teste/aula11.py
@@ -16,9 +16,7 @@ print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome, 
 
 #outro exemplo de como executar 
 nome = 'Vinicius'
-cores = {'limpa':'\033[m', 
-         'azul':'\033[34m', 
-         'amarelo':'\033[33m', 
-         'pretoebranco':'\033[7;30m'}
+cores = {'limpa':'\033[m', 'azul':'\033[34m', 'amarelo':'\033[33m', 
+'pretoebranco':'\033[7;30m'}
 
 print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format(cores['amarelo'], nome, cores['limpa']))
