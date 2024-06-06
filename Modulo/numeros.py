fb4ef73a2cc6ca1f8fd5dbb76ed0d2a9735976b4
Vinicius Collaço <viniciuscollaco@hotmail.com>
2024-06-06 20:34:54 -0300

    Modulos aula

--- Modulo/numeros.py
+++ Modulo/numeros.py
@@ -1,9 +1,6 @@
-def fatorial(n):
-    f = 1
-    for c in range(1, n+1):
-        f *= c
-    return f
-
+from uteis import fatorial, dobro, triplo
 num = int(input('Digite um valor '))
 fat = fatorial(num) 
 print(f'O fatorial de {num} é {fat}')
+print(f'O drobro de {num} é {dobro(num)}')
+print(f'O triplo de {num} é {triplo(num)}')
