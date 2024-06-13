db0f9c0eb2906ef1e316f40968fd2329c6fe87cb
Vinicius Collaço <viniciuscollaco@hotmail.com>
2024-06-13 20:07:14 -0300

    Wxwecicio 107 de modulos

---
+++ Desafios/Tarefas/moeda.py
@@ -0,0 +1,17 @@
+def aumentar(valor):
+    porcentAum = valor * 0.1
+    resulAum = porcentAum + valor
+    return resulAum
+    
+def diminuir(valor):
+    porcentDim = valor * 0.13
+    resulDim = valor - porcentDim
+    return resulDim
+    
+def dobro(valor):
+    resulDobr = valor * 2
+    return resulDobr
+    
+def metade(valor):
+    resultMeta = valor / 2
+    return resultMeta
