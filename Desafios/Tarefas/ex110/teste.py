786b6cb35e50c7782e4ab9fa87e6f6b64f3e8b18
Vinicius Collaço <viniciuscollaco@hotmail.com>
2024-06-26 16:35:37 -0300

    Exercicios de pacotes

---
+++ Desafios/Tarefas/ex110/teste.py
@@ -0,0 +1,3 @@
+import moeda 
+valor = float(input('Digite o preço: R$ '))
+moeda.resumo(valor, 80, 35)
---
+++ Desafios/Tarefas/ex111/teste.py
@@ -0,0 +1,3 @@
+from utilidadesCeV import moeda 
+valor = float(input('Digite o preço: R$ '))
+moeda.resumo(valor, 80, 35)
